ab439bf4fafd711a93a6d7a07d170486bd42151e
Michel-Chamarez <michel-chamarez@Imutes.wind3.hub>
2022-08-06 17:59:53 +0200

    feat: architecture and libraries

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,7 +6,11 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/test'), AuthModule, UsersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
